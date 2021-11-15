353e67c7176e0ab88192c4798808e120bd222d1b
markgardie <markgardie@users.noreply.github.com>
2021-11-15 19:19:44 +0200

    design layout for placeholder

--- app/src/main/java/com/markgardie/graduatework/RecipesFragment.kt
+++ app/src/main/java/com/markgardie/graduatework/RecipesFragment.kt
@@ -5,15 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_recipes.view.*
 
 class RecipesFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_recipes, container, false)
+        val view = inflater.inflate(R.layout.fragment_recipes, container, false)
+
+        return view
     }
 
 
