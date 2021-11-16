627a88b032e9d9ae091fd90e12f9fb5db402b022
markgardie <markgardie@users.noreply.github.com>
2021-11-16 17:17:31 +0200

    add constants class for base url and api key constants

---
+++ app/src/main/java/com/markgardie/graduatework/Constants.kt
@@ -0,0 +1,9 @@
+package com.markgardie.graduatework
+
+class Constants {
+
+    companion object {
+        const val API_KEY = "c9bb43e5ef4f479fb41e55e3d11b760f"
+        const val BASE_URL = "https://api.spoonacular.com"
+    }
+}
