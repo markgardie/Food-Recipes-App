61db9fe9bf806dfbcdde01b94f1b031ec90fc66a
markgardie <markgardie@users.noreply.github.com>
2022-01-25 19:29:53 +0200

    inject FoodRecipesApi into RemoteDataSource

--- app/src/main/java/com/markgardie/graduatework/RemoteDataSource.kt
+++ app/src/main/java/com/markgardie/graduatework/RemoteDataSource.kt
@@ -1,5 +1,15 @@
 package com.markgardie.graduatework
 
-class RemoteDataSource() {
+import com.markgardie.graduatework.models.FoodRecipe
+import retrofit2.Response
+import javax.inject.Inject
+
+class RemoteDataSource @Inject constructor(
+    private val foodRecipesApi: FoodRecipesApi
+    ) {
+
+    suspend fun getRecipes(queries: Map<String, String>): Response<FoodRecipe> {
+        return foodRecipesApi.getRecipes(queries)
+    }
 
 }
