da457618498f3aeb30a6e4a59b9272f651d932ac
markgardie <markgardie@users.noreply.github.com>
2022-02-02 21:25:58 +0200

    add function to setup RecyclerView

--- app/src/main/java/com/markgardie/graduatework/ui/fragments/recipes/RecipesFragment.kt
+++ app/src/main/java/com/markgardie/graduatework/ui/fragments/recipes/RecipesFragment.kt
@@ -5,11 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.markgardie.graduatework.R
+import com.markgardie.graduatework.adapters.RecipesAdapter
 import kotlinx.android.synthetic.main.fragment_recipes.view.*
 
 class RecipesFragment : Fragment() {
 
+    private val mAdapter by lazy { RecipesAdapter() }
     private lateinit var mView: View;
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
@@ -17,7 +20,17 @@ class RecipesFragment : Fragment() {
         // Inflate the layout for this fragment
         mView = inflater.inflate(R.layout.fragment_recipes, container, false)
 
+        setupRecyclerView()
+
         return mView
+    }
+
+
+
+    private fun setupRecyclerView() {
+        mView.recyclerview.adapter = mAdapter
+        mView.recyclerview.layoutManager = LinearLayoutManager(requireContext())
+        showShimmerEffect()
     }
 
     private fun showShimmerEffect() {
