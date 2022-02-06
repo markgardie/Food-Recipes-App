bdfcd254ffa1741f825adfb0347b364bc5c648a9
markgardie <markgardie@users.noreply.github.com>
2022-02-06 20:39:59 +0200

    create TypeConverter

---
+++ app/src/main/java/com/markgardie/graduatework/RecipesTypeConverter.kt
@@ -0,0 +1,23 @@
+package com.markgardie.graduatework
+
+import androidx.room.TypeConverter
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import com.markgardie.graduatework.models.FoodRecipe
+
+class RecipesTypeConverter {
+
+    var gson = Gson()
+
+    @TypeConverter
+    fun foodRecipeToString(foodRecipe: FoodRecipe) : String {
+        return gson.toJson(foodRecipe)
+    }
+
+    @TypeConverter
+    fun stringToFoodRecipe(data: String): FoodRecipe {
+        val lisType = object : TypeToken<FoodRecipe>() {}.type
+        return gson.fromJson(data, lisType)
+    }
+
+}
