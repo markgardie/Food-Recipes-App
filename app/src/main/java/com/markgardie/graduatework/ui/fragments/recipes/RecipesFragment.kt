9884f99f85e9e49eca83bc86ef22df0f31c46d70
markgardie <markgardie@users.noreply.github.com>
2022-03-14 19:39:37 +0200

    no internet crush bug fixed

--- app/src/main/java/com/markgardie/graduatework/ui/fragments/recipes/RecipesFragment.kt
+++ app/src/main/java/com/markgardie/graduatework/ui/fragments/recipes/RecipesFragment.kt
@@ -56,6 +56,7 @@ class RecipesFragment : Fragment() {
         binding.mainViewModel = mainViewModel
 
         setupRecyclerView()
+        readDatabase()
 
         recipesViewModel.readBackOnline.observe(viewLifecycleOwner) {
             recipesViewModel.backOnline = it
@@ -68,7 +69,7 @@ class RecipesFragment : Fragment() {
                         Log.d("NetworkListener", status.toString())
                         recipesViewModel.networkStatus = status
                         recipesViewModel.showNetworkStatus()
-                        readDatabase()
+
                     }
         }
 
