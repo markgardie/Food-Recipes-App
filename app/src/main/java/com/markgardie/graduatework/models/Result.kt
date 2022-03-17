81c8afc5c930f07d64f25d636c98ee3e831e1554
markgardie <markgardie@users.noreply.github.com>
2022-03-17 16:28:15 +0200

    send result to DetailsActivity

--- app/src/main/java/com/markgardie/graduatework/models/Result.kt
+++ app/src/main/java/com/markgardie/graduatework/models/Result.kt
@@ -3,7 +3,11 @@ package com.markgardie.graduatework.models
 
 import com.google.gson.annotations.SerializedName
 import com.markgardie.graduatework.models.ExtendedIngredient
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+import kotlinx.android.parcel.RawValue
 
+@Parcelize
 data class Result(
         @SerializedName("aggregateLikes")
         val aggregateLikes: Int,
@@ -12,7 +16,7 @@ data class Result(
         @SerializedName("dairyFree")
         val dairyFree: Boolean,
         @SerializedName("extendedIngredients")
-        val extendedIngredients: List<ExtendedIngredient>,
+        val extendedIngredients: @RawValue List<ExtendedIngredient>,
         @SerializedName("glutenFree")
         val glutenFree: Boolean,
         @SerializedName("id")
@@ -35,4 +39,4 @@ data class Result(
         val vegetarian: Boolean,
         @SerializedName("veryHealthy")
         val veryHealthy: Boolean
-)
+): Parcelable
