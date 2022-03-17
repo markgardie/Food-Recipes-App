972760146724e001f2f35e19e0b628ce0c75ab94
markgardie <markgardie@users.noreply.github.com>
2022-03-17 15:47:15 +0200

    create empty DetailsActivity.kt

---
+++ app/src/main/java/com/markgardie/graduatework/ui/DetailsActivity.kt
@@ -0,0 +1,12 @@
+package com.markgardie.graduatework.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.markgardie.graduatework.R
+
+class DetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_details)
+    }
+}
