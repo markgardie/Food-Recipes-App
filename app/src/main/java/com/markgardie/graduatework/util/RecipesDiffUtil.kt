ee4f1a64fed4552225af9f43823b8d53cb6ef257
markgardie <markgardie@users.noreply.github.com>
2022-03-24 18:06:22 +0200

    add generics to RecipesDiffUtil.kt

--- app/src/main/java/com/markgardie/graduatework/util/RecipesDiffUtil.kt
+++ app/src/main/java/com/markgardie/graduatework/util/RecipesDiffUtil.kt
@@ -3,9 +3,9 @@ package com.markgardie.graduatework.util
 import androidx.recyclerview.widget.DiffUtil
 import com.markgardie.graduatework.models.Result
 
-class RecipesDiffUtil(
-        private val oldList: List<Result>,
-        private val newList: List<Result>
+class RecipesDiffUtil<T>(
+        private val oldList: List<T>,
+        private val newList: List<T>
 ) : DiffUtil.Callback() {
 
 
