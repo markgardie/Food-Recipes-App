eedfabde79036edce39f9365e4fc3190fdff6cec
markgardie <markgardie@users.noreply.github.com>
2022-03-25 16:59:06 +0200

    display ingredients data

--- app/src/main/java/com/markgardie/graduatework/ui/fragments/overview/OverviewFragment.kt
+++ app/src/main/java/com/markgardie/graduatework/ui/fragments/overview/OverviewFragment.kt
@@ -9,6 +9,7 @@ import com.markgardie.graduatework.R
 import coil.load
 import androidx.core.content.ContextCompat
 import com.markgardie.graduatework.models.Result
+import com.markgardie.graduatework.util.Constants.Companion.RECIPE_RESULT_KEY
 import kotlinx.android.synthetic.main.fragment_overview.view.*
 import org.jsoup.Jsoup
 
@@ -23,7 +24,7 @@ class OverviewFragment : Fragment() {
         val view = inflater.inflate(R.layout.fragment_overview, container, false)
 
         val args = arguments
-        val myBundle: Result? = args?.getParcelable("recipeBundle")
+        val myBundle: Result? = args?.getParcelable(RECIPE_RESULT_KEY)
 
         view.main_imageView.load(myBundle?.image)
         view.title_textView.text = myBundle?.title
--- app/src/main/java/com/markgardie/graduatework/util/Constants.kt
+++ app/src/main/java/com/markgardie/graduatework/util/Constants.kt
@@ -7,6 +7,8 @@ class Constants {
         const val BASE_URL = "https://api.spoonacular.com"
         const val BASE_IMAGE_URL = "https://spoonacular.com/cdn/ingredients_100x100/"
 
+        const val RECIPE_RESULT_KEY = "recipeBundle"
+
         //API Query Keys
         const val QUERY_SEARCH = "query"
         const val QUERY_NUMBER = "number"
