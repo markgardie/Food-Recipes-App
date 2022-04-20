b784f420fab1c82edf233256b4994df68b7f21c2
markgardie <markgardie@users.noreply.github.com>
2022-04-20 19:01:45 +0300

    single click listener

--- app/src/main/java/com/markgardie/graduatework/adapters/FavoriteRecipesAdapter.kt
+++ app/src/main/java/com/markgardie/graduatework/adapters/FavoriteRecipesAdapter.kt
@@ -2,11 +2,14 @@ package com.markgardie.graduatework.adapters
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.markgardie.graduatework.data.database.entities.FavoritesEntity
 import com.markgardie.graduatework.databinding.FavoritesRecipesRowLayoutBinding
+import com.markgardie.graduatework.ui.fragments.favorites.FavoriteRecipesFragmentDirections
 import com.markgardie.graduatework.util.RecipesDiffUtil
+import kotlinx.android.synthetic.main.favorites_recipes_row_layout.view.*
 
 
 class FavoriteRecipesAdapter : RecyclerView.Adapter<FavoriteRecipesAdapter.MyViewHolder>() {
@@ -38,6 +41,14 @@ class FavoriteRecipesAdapter : RecyclerView.Adapter<FavoriteRecipesAdapter.MyVie
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         val selectedRecipe = favoriteRecipes[position]
         holder.bind(selectedRecipe)
+
+        holder.itemView.favoriteRecipesRowLayout.setOnClickListener {
+            val action =
+                    FavoriteRecipesFragmentDirections.actionFavoriteRecipesFragmentToDetailsActivity(
+                            selectedRecipe.result
+                    )
+            holder.itemView.findNavController().navigate(action)
+        }
     }
 
     override fun getItemCount(): Int {
