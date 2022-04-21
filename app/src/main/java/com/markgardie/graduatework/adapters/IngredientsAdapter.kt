eef6d0d3f0fb35f6ded7a904042363fcb50f77d1
markgardie <markgardie@users.noreply.github.com>
2022-04-21 17:27:40 +0300

    add databinding to IngredientsAdapter.kt

--- app/src/main/java/com/markgardie/graduatework/adapters/IngredientsAdapter.kt
+++ app/src/main/java/com/markgardie/graduatework/adapters/IngredientsAdapter.kt
@@ -1,30 +1,55 @@
 package com.markgardie.graduatework.adapters
 
+import android.util.Log
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import coil.load
 import com.markgardie.graduatework.R
+import com.markgardie.graduatework.databinding.IngredientsRowLayoutBinding
 import com.markgardie.graduatework.models.ExtendedIngredient
+import com.markgardie.graduatework.ui.fragments.ingredients.IngredientsFragmentDirections
 import com.markgardie.graduatework.util.Constants.Companion.BASE_IMAGE_URL
 import com.markgardie.graduatework.util.RecipesDiffUtil
 import kotlinx.android.synthetic.main.ingredients_row_layout.view.*
-import java.util.*
+import java.lang.Exception
 
 
-class IngredientsAdapter: RecyclerView.Adapter<IngredientsAdapter.MyViewHolder>() {
+
+class IngredientsAdapter(): RecyclerView.Adapter<IngredientsAdapter.MyViewHolder>() {
 
     private var ingredientsList = emptyList<ExtendedIngredient>()
 
-    class MyViewHolder(itemView: View): RecyclerView.ViewHolder(itemView)
+    class MyViewHolder(private val binding: IngredientsRowLayoutBinding): RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(extendedIngredient: ExtendedIngredient) {
+            binding.extendedIngredient = extendedIngredient
+            binding.executePendingBindings()
+        }
+
+        companion object {
+            fun from(parent: ViewGroup): MyViewHolder {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val binding = IngredientsRowLayoutBinding.inflate(layoutInflater, parent, false)
+                return MyViewHolder(binding)
+            }
+        }
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-        return MyViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.ingredients_row_layout, parent, false))
+        return MyViewHolder.from(parent)
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        val currentIngredient = ingredientsList[position]
+        holder.bind(currentIngredient)
+
         holder.itemView.ingredient_imageView.load(BASE_IMAGE_URL + ingredientsList[position].image) {
             crossfade(600)
             error(R.drawable.ic_error_placeholder)
@@ -34,6 +59,8 @@ class IngredientsAdapter: RecyclerView.Adapter<IngredientsAdapter.MyViewHolder>(
         holder.itemView.ingredient_unit.text = ingredientsList[position].unit
         holder.itemView.ingredient_consistency.text = ingredientsList[position].consistency
         holder.itemView.ingredient_original.text = ingredientsList[position].original
+
+
     }
 
     override fun getItemCount(): Int {
