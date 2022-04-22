dd6ae87464471da8747f89719a236ccaad1dbb99
markgardie <markgardie@users.noreply.github.com>
2022-04-22 13:56:24 +0300

    add args from IngredientFragment to PriceActivity

--- app/src/main/java/com/markgardie/graduatework/ui/PriceActivity.kt
+++ app/src/main/java/com/markgardie/graduatework/ui/PriceActivity.kt
@@ -2,9 +2,13 @@ package com.markgardie.graduatework.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.navArgs
 import com.markgardie.graduatework.R
 
 class PriceActivity : AppCompatActivity() {
+
+    private val args by navArgs<DetailsActivityArgs>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_price)
