5dd7a18faf80e343db1a13bd288f9f85032fc5b9
markgardie <markgardie@users.noreply.github.com>
2022-04-28 20:30:27 +0300

    add products functionality to MainViewModel.kt

--- app/src/main/java/com/markgardie/graduatework/viewmodels/MainViewModel.kt
+++ app/src/main/java/com/markgardie/graduatework/viewmodels/MainViewModel.kt
@@ -10,6 +10,7 @@ import com.markgardie.graduatework.data.Repository
 import com.markgardie.graduatework.data.database.entities.FavoritesEntity
 import com.markgardie.graduatework.data.database.entities.RecipesEntity
 import com.markgardie.graduatework.models.FoodRecipe
+import com.markgardie.graduatework.models.Product
 import com.markgardie.graduatework.util.NetworkResult
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -48,9 +49,14 @@ class MainViewModel @ViewModelInject constructor (
     /** RETROFIT */
     var recipesResponse: MutableLiveData<NetworkResult<FoodRecipe>> = MutableLiveData()
     var searchedRecipesResponse: MutableLiveData<NetworkResult<FoodRecipe>> = MutableLiveData()
+    var productsResponse: MutableLiveData<NetworkResult<Product>> = MutableLiveData()
 
     fun getRecipes(queries: Map<String, String>) = viewModelScope.launch {
         getRecipesSafeCall(queries)
+    }
+
+    fun getProducts(shopId: String, title: String) = viewModelScope.launch {
+        getProductsSafeCall(shopId, title)
     }
 
     fun searchRecipes(searchQuery: Map<String, String>) = viewModelScope.launch {
@@ -73,6 +79,21 @@ class MainViewModel @ViewModelInject constructor (
             }
         } else {
             recipesResponse.value = NetworkResult.Error("No Internet Connection.")
+        }
+    }
+
+    private suspend fun getProductsSafeCall(shopId: String, title: String) {
+        productsResponse.value = NetworkResult.Loading()
+        if (hasInternetConnection()) {
+            try {
+                val response = repository.remote.getProducts(shopId, title)
+                productsResponse.value = handleProductResponse(response)
+
+            } catch (e: Exception) {
+                productsResponse.value = NetworkResult.Error("Products not found.")
+            }
+        } else {
+            productsResponse.value = NetworkResult.Error("No Internet Connection.")
         }
     }
 
@@ -110,6 +131,24 @@ class MainViewModel @ViewModelInject constructor (
             response.isSuccessful -> {
                 val foodRecipes = response.body()
                 return NetworkResult.Success(foodRecipes!!)
+            }
+            else -> {
+                return NetworkResult.Error(response.message())
+            }
+        }
+    }
+
+    private fun handleProductResponse(response: Response<Product>): NetworkResult<Product>? {
+        when {
+            response.message().toString().contains("timeout") -> {
+                return NetworkResult.Error("Timeout")
+            }
+            response.body()!!.ean.isNullOrEmpty() -> {
+                return NetworkResult.Error("Products not found.")
+            }
+            response.isSuccessful -> {
+                val products = response.body()
+                return NetworkResult.Success(products!!)
             }
             else -> {
                 return NetworkResult.Error(response.message())
