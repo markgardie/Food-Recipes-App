c66708b3c831e7b05f3507b285da6a214bcd6eac
markgardie <markgardie@users.noreply.github.com>
2022-04-28 20:10:05 +0300

    update NetworkModule.kt with Zakaz.ua api

--- app/src/main/java/com/markgardie/graduatework/di/NetworkModule.kt
+++ app/src/main/java/com/markgardie/graduatework/di/NetworkModule.kt
@@ -2,6 +2,8 @@ package com.markgardie.graduatework.di
 
 import com.markgardie.graduatework.util.Constants.Companion.BASE_URL
 import com.markgardie.graduatework.data.network.FoodRecipesApi
+import com.markgardie.graduatework.data.network.ProductsApi
+import com.markgardie.graduatework.util.Constants.Companion.PRODUCTS_URL
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -10,6 +12,7 @@ import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
+import javax.inject.Qualifier
 import javax.inject.Singleton
 
 @Module
@@ -31,8 +34,16 @@ object NetworkModule {
         return GsonConverterFactory.create()
     }
 
+    @Qualifier
+    @Retention(AnnotationRetention.BINARY)
+    annotation class SpoonacularRetrofit
+
+    @Qualifier
+    @Retention(AnnotationRetention.BINARY)
+    annotation class ZakazUaRetrofit
 
     @Singleton
+    @SpoonacularRetrofit
     @Provides
     fun provideSpoonacularRetrofitInstance(okHttpClient: OkHttpClient,
                                            gsonConverterFactory: GsonConverterFactory) : Retrofit {
@@ -44,10 +55,29 @@ object NetworkModule {
 
     }
 
+
     @Singleton
     @Provides
-    fun provideSpoonacularApiService(retrofit: Retrofit): FoodRecipesApi {
+    fun provideSpoonacularApiService(@SpoonacularRetrofit retrofit: Retrofit): FoodRecipesApi {
         return retrofit.create(FoodRecipesApi::class.java)
+    }
+
+    @Singleton
+    @ZakazUaRetrofit
+    @Provides
+    fun provideZakazUaRetrofitInstance(okHttpClient: OkHttpClient,
+                                       gsonConverterFactory: GsonConverterFactory) : Retrofit {
+        return Retrofit.Builder()
+                .baseUrl(PRODUCTS_URL)
+                .client(okHttpClient)
+                .addConverterFactory(gsonConverterFactory)
+                .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideZakazUaApiService(@ZakazUaRetrofit retrofit: Retrofit): ProductsApi {
+        return retrofit.create(ProductsApi::class.java)
     }
 
 }
