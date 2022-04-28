514bb2aab90f650c6207e3885862d5e4edc758a4
markgardie <markgardie@users.noreply.github.com>
2022-04-28 18:27:06 +0300

    create zakaz.ua api

---
+++ app/src/main/java/com/markgardie/graduatework/data/network/ProductsApi.kt
@@ -0,0 +1,18 @@
+package com.markgardie.graduatework.data.network
+
+import com.markgardie.graduatework.models.Product
+import com.markgardie.graduatework.util.Constants.Companion.EKO_MARKET
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface ProductsApi {
+
+    @GET("/{shop_id}/products/search")
+    suspend fun getProducts(
+            @Path("shop_id") shopId: String,
+            @Query("title") title: String
+    ): Response<Product>
+
+}
