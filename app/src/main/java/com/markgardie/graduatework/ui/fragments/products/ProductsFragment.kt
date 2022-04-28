a0e09db5cc81c6fe2df0faea392fa7ca2699e756
markgardie <markgardie@users.noreply.github.com>
2022-04-28 17:08:06 +0300

    rename PriceFragment to ProductFragment

--- app/src/main/java/com/markgardie/graduatework/ui/fragments/products/ProductsFragment.kt
+++ app/src/main/java/com/markgardie/graduatework/ui/fragments/products/ProductsFragment.kt
@@ -1,4 +1,4 @@
-package com.markgardie.graduatework.ui.fragments.prices
+package com.markgardie.graduatework.ui.fragments.products
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -8,12 +8,12 @@ import android.view.ViewGroup
 import com.markgardie.graduatework.R
 
 
-class PricesFragment : Fragment() {
+class ProductsFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_prices, container, false)
+        return inflater.inflate(R.layout.fragment_products, container, false)
     }
 
 
