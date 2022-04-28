b8099e5ec257809c1f75c33e305873d84c708e88
markgardie <markgardie@users.noreply.github.com>
2022-04-28 17:18:04 +0300

    add Product model class

---
+++ app/src/main/java/com/markgardie/graduatework/models/Product.kt
@@ -0,0 +1,19 @@
+package com.markgardie.graduatework.models
+
+import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
+import kotlinx.android.parcel.Parcelize
+import java.util.*
+
+@Parcelize
+data class Product(
+        @SerializedName("title")
+        val title: String,
+        @SerializedName("price")
+        val price: Int,
+        @SerializedName("currency")
+        val currency: String,
+        @SerializedName("image")
+        val image: String
+
+) : Parcelable
