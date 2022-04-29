50b95db73fb44d0e40fb3446797bef9ba40f810f
markgardie <markgardie@users.noreply.github.com>
2022-04-29 19:31:38 +0300

    add safe args to start destinations - ProductsFragment.kt

--- app/src/main/java/com/markgardie/graduatework/ui/PriceActivity.kt
+++ app/src/main/java/com/markgardie/graduatework/ui/PriceActivity.kt
@@ -2,17 +2,31 @@ package com.markgardie.graduatework.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
 import androidx.navigation.navArgs
 import com.markgardie.graduatework.R
+import com.markgardie.graduatework.ui.fragments.products.ProductsFragment
+import com.markgardie.graduatework.util.Constants.Companion.INGREDIENTS_BUNDLE
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 
 @AndroidEntryPoint
 class PriceActivity : AppCompatActivity() {
 
+    private val args by navArgs<PriceActivityArgs>()
 
+    @ExperimentalCoroutinesApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_price)
+
+        val ingredientsBundle = Bundle()
+        ingredientsBundle.putParcelable(INGREDIENTS_BUNDLE, args.ingredient)
+
+        val navController = findNavController(R.id.priceActivityNavHostFragment)
+        navController.setGraph(R.navigation.price_activity_nav, ingredientsBundle)
 
     }
 }
