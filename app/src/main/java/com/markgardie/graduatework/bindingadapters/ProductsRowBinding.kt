64012c293688a21c2c718e051ed5492a71562d82
markgardie <markgardie@users.noreply.github.com>
2022-04-29 17:21:10 +0300

    add Products Binding Adapter

---
+++ app/src/main/java/com/markgardie/graduatework/bindingadapters/ProductsRowBinding.kt
@@ -0,0 +1,40 @@
+package com.markgardie.graduatework.bindingadapters
+
+import android.util.Log
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
+import androidx.databinding.BindingAdapter
+import androidx.navigation.findNavController
+import coil.load
+import com.markgardie.graduatework.R
+import com.markgardie.graduatework.models.Result
+import com.markgardie.graduatework.ui.fragments.recipes.RecipesFragmentDirections
+import org.jsoup.Jsoup
+import java.lang.Exception
+
+class ProductsRowBinding {
+
+    companion object {
+
+
+        @BindingAdapter("loadImageFromUrl")
+        @JvmStatic
+        fun loadImageFromUrl(imageView: ImageView, imageUrl: String) {
+            imageView.load(imageUrl) {
+                crossfade(600)
+                error(R.drawable.ic_error_placeholder)
+            }
+        }
+
+
+        @BindingAdapter("setPrice")
+        @JvmStatic
+        fun setPrice(textView: TextView, price: Int) {
+            textView.text = price.toString()
+        }
+
+    }
+}
