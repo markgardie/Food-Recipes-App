94f05729fe7cdfec8fc8ee76139df522d5f2b0c4
markgardie <markgardie@users.noreply.github.com>
2022-05-03 16:12:29 +0300

    rename activity

--- app/src/main/java/com/markgardie/graduatework/ui/ProductsActivity.kt
+++ app/src/main/java/com/markgardie/graduatework/ui/ProductsActivity.kt
@@ -2,25 +2,22 @@ package com.markgardie.graduatework.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.navigation.NavController
-import androidx.navigation.Navigation
 import androidx.navigation.findNavController
 import androidx.navigation.navArgs
 import com.markgardie.graduatework.R
-import com.markgardie.graduatework.ui.fragments.products.ProductsFragment
 import com.markgardie.graduatework.util.Constants.Companion.INGREDIENTS_BUNDLE
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 
 @AndroidEntryPoint
-class PriceActivity : AppCompatActivity() {
+class ProductsActivity : AppCompatActivity() {
 
-    private val args by navArgs<PriceActivityArgs>()
+    private val args by navArgs<ProductsActivityArgs>()
 
     @ExperimentalCoroutinesApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_price)
+        setContentView(R.layout.activity_products)
 
         val ingredientsBundle = Bundle()
         ingredientsBundle.putParcelable(INGREDIENTS_BUNDLE, args.ingredient)
