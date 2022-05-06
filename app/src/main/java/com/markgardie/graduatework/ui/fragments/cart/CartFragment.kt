06dbcee18c5b331eb9dd4105afe8a5e4276f1f75
markgardie <markgardie@users.noreply.github.com>
2022-05-06 19:30:40 +0300

    setting CartFragment.kt

--- app/src/main/java/com/markgardie/graduatework/ui/fragments/cart/CartFragment.kt
+++ app/src/main/java/com/markgardie/graduatework/ui/fragments/cart/CartFragment.kt
@@ -1,21 +1,41 @@
 package com.markgardie.graduatework.ui.fragments.cart
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.markgardie.graduatework.R
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.markgardie.graduatework.adapters.CartAdapter
+import com.markgardie.graduatework.databinding.FragmentCartBinding
+import com.markgardie.graduatework.viewmodels.MainViewModel
 
 
 class CartFragment : Fragment() {
+
+    private val mainViewModel: MainViewModel by viewModels()
+    private val mAdapter: CartAdapter by lazy { CartAdapter() }
+
+    private var _binding: FragmentCartBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cart, container, false)
+        _binding = FragmentCartBinding.inflate(inflater, container, false)
+        binding.lifecycleOwner = this
+
+        setupRecyclerView(binding.cartRecyclerView)
+
+        return binding.root
+    }
+
+    private fun setupRecyclerView(recyclerView: RecyclerView) {
+        recyclerView.adapter = mAdapter
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
     }
 
 }
