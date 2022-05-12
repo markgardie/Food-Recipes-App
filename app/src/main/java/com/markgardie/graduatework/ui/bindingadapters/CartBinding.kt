17332956698992fb95e5711c9f9d3f41219ba578
markgardie <markgardie@users.noreply.github.com>
2022-05-12 19:29:53 +0300

    add new adapter functions

--- app/src/main/java/com/markgardie/graduatework/ui/bindingadapters/CartBinding.kt
+++ app/src/main/java/com/markgardie/graduatework/ui/bindingadapters/CartBinding.kt
@@ -1,8 +1,14 @@
 package com.markgardie.graduatework.ui.bindingadapters
 
+import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.markgardie.graduatework.data.database.entities.FavoritesEntity
 import com.markgardie.graduatework.data.database.entities.ProductEntity
+import com.markgardie.graduatework.ui.adapters.CartAdapter
+import com.markgardie.graduatework.ui.adapters.FavoriteRecipesAdapter
 
 class CartBinding {
 
@@ -20,6 +26,41 @@ class CartBinding {
             }
 
             textView.text = totalPrice.toString()
+        }
+
+        @BindingAdapter("cartViewVisibility", "cartSetData", requireAll = false)
+        @JvmStatic
+        fun setDataAndViewVisibility(
+            view: View,
+            productEntity: List<ProductEntity>?,
+            mAdapter: CartAdapter?
+        ) {
+            if (productEntity.isNullOrEmpty()) {
+                when (view) {
+                    is ImageView -> {
+                        view.visibility = View.VISIBLE
+                    }
+                    is TextView -> {
+                        view.visibility = View.VISIBLE
+                    }
+                    is RecyclerView -> {
+                        view.visibility = View.INVISIBLE
+                    }
+                }
+            } else {
+                when (view) {
+                    is ImageView -> {
+                        view.visibility = View.INVISIBLE
+                    }
+                    is TextView -> {
+                        view.visibility = View.INVISIBLE
+                    }
+                    is RecyclerView -> {
+                        view.visibility = View.VISIBLE
+                        mAdapter?.setData(productEntity)
+                    }
+                }
+            }
         }
     }
 }
