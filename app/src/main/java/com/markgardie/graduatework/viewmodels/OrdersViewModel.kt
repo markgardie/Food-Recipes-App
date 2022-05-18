4ada9fa36b3f1e0cc12fd0ef15dd2d2ae2130999
markgardie <markgardie@users.noreply.github.com>
2022-05-18 13:54:51 +0300

    set ViewModel for orders functionality

---
+++ app/src/main/java/com/markgardie/graduatework/viewmodels/OrdersViewModel.kt
@@ -0,0 +1,37 @@
+package com.markgardie.graduatework.viewmodels
+
+import android.app.Application
+import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
+import com.markgardie.graduatework.data.Repository
+import com.markgardie.graduatework.data.database.entities.OrdersEntity
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+
+class OrdersViewModel @ViewModelInject constructor(
+        private val repository: Repository,
+        application: Application
+) : AndroidViewModel(application) {
+
+
+    val readOrders: LiveData<List<OrdersEntity>> = repository.local.readOrders().asLiveData()
+
+    private fun addOrder(ordersEntity: OrdersEntity) =
+            viewModelScope.launch(Dispatchers.IO) {
+                repository.local.addOrder(ordersEntity)
+            }
+
+    private fun deleteOrder(ordersEntity: OrdersEntity) =
+            viewModelScope.launch(Dispatchers.IO) {
+                repository.local.deleteOrder(ordersEntity)
+            }
+
+    private fun deleteAllOrders() =
+            viewModelScope.launch(Dispatchers.IO) {
+                repository.local.deleteAllOrders()
+            }
+}
