ed3a65a2c341fed5bf5fe55e8533ae59b620cc73
markgardie <markgardie@users.noreply.github.com>
2022-05-23 20:23:50 +0300

    add OrderFragment.kt

---
+++ app/src/main/java/com/markgardie/graduatework/ui/fragments/OrderFragment.kt
@@ -0,0 +1,19 @@
+package com.markgardie.graduatework.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.markgardie.graduatework.R
+
+class OrderFragment : Fragment() {
+
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_order, container, false)
+    }
+
+}
