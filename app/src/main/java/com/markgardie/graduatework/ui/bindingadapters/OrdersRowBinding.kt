55a5764e48bcbbee6e711eade2efddec35e213f7
markgardie <markgardie@users.noreply.github.com>
2022-05-25 11:53:46 +0300

    add Order Total Price adapter

--- app/src/main/java/com/markgardie/graduatework/ui/bindingadapters/OrdersRowBinding.kt
+++ app/src/main/java/com/markgardie/graduatework/ui/bindingadapters/OrdersRowBinding.kt
@@ -4,6 +4,9 @@ import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import com.markgardie.graduatework.R
 import com.markgardie.graduatework.data.database.entities.OrdersEntity
+import com.markgardie.graduatework.models.ProductsList
+import java.math.BigDecimal
+import java.math.RoundingMode
 
 class OrdersRowBinding {
 
@@ -30,6 +33,21 @@ class OrdersRowBinding {
             textView.text = "Delivery Address: ${ordersEntity.address}"
         }
 
+        @BindingAdapter("setTotalPrice")
+        @JvmStatic
+        fun setTotalPrice(textView: TextView, products: ProductsList) {
+            var totalPrice = 0.0
+
+            products.products.forEach {
+                totalPrice += it.price
+            }
+
+            totalPrice /= 100.toDouble()
+            val totalPriceRounded = BigDecimal(totalPrice).setScale(2, RoundingMode.HALF_EVEN)
+
+            textView.text = "Total Price: ${totalPriceRounded}"
+
+        }
 
     }
 }
