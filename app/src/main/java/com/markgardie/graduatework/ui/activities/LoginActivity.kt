fdb965eb80e831c26b6b2c9cb4e0d8dca4dcd571
markgardie <markgardie@users.noreply.github.com>
2022-06-21 14:00:50 +0300

    add functions into LoginActivity.kt

--- app/src/main/java/com/markgardie/graduatework/ui/activities/LoginActivity.kt
+++ app/src/main/java/com/markgardie/graduatework/ui/activities/LoginActivity.kt
@@ -1,12 +1,71 @@
 package com.markgardie.graduatework.ui.activities
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.ErrorCodes
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
 import com.markgardie.graduatework.R
+import com.markgardie.graduatework.util.Constants.Companion.REQUEST_CODE_SIGN_IN
 
 class LoginActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        createSignInIntent()
     }
+
+    private fun createSignInIntent() {
+        val providers = arrayListOf<AuthUI.IdpConfig>(
+            AuthUI.IdpConfig.EmailBuilder().build(),
+            AuthUI.IdpConfig.PhoneBuilder().build(),
+            AuthUI.IdpConfig.GoogleBuilder().build()
+        )
+        startActivityForResult(
+            AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .setIsSmartLockEnabled(false)
+                .build(),
+            REQUEST_CODE_SIGN_IN
+        )
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == REQUEST_CODE_SIGN_IN){
+            var response = IdpResponse.fromResultIntent(data)
+            if (resultCode == Activity.RESULT_OK){
+                val user = FirebaseAuth.getInstance().currentUser
+                val intent = Intent(this,MainActivity::class.java)
+                startActivity(intent)
+
+            }
+
+            else{
+
+                if(response == null){
+                    finish()
+                }
+                if (response?.error?.errorCode == ErrorCodes.NO_NETWORK) {
+                    //Show No Internet Notification
+                    return
+                }
+
+                if(response?.error?.errorCode == ErrorCodes.UNKNOWN_ERROR) {
+                    Toast.makeText(this, response?.error?.errorCode.toString(), Toast.LENGTH_LONG)
+                        .show()
+                    Log.d("ERRORCODE", response?.error?.errorCode.toString())
+                    return
+                }
+            }
+        }
+    }
+
 }
