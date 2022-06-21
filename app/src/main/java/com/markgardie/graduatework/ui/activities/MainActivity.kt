5d4552b340eb07ee9ba267c1c07328fd80456d40
markgardie <markgardie@users.noreply.github.com>
2022-06-21 17:15:44 +0300

    add auth functions

--- app/src/main/java/com/markgardie/graduatework/ui/activities/MainActivity.kt
+++ app/src/main/java/com/markgardie/graduatework/ui/activities/MainActivity.kt
@@ -2,11 +2,18 @@ package com.markgardie.graduatework.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.ErrorCodes
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
+import com.google.firebase.auth.FirebaseAuth
 import com.markgardie.graduatework.R
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.activity_main.*
@@ -16,18 +23,71 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var navController: NavController
 
+    private val signInLauncher = registerForActivityResult(
+        FirebaseAuthUIActivityResultContract()
+    ) { res ->
+        this.onSignInResult(res)
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        createSignInIntent()
+    }
 
+    private fun createSignInIntent() {
+
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.EmailBuilder().build(),
+            AuthUI.IdpConfig.PhoneBuilder().build(),
+            AuthUI.IdpConfig.GoogleBuilder().build()
+        )
+
+        val signInIntent = AuthUI.getInstance()
+            .createSignInIntentBuilder()
+            .setAvailableProviders(providers)
+            .build()
+        signInLauncher.launch(signInIntent)
+
+    }
+
+    private fun onSignInResult(result: FirebaseAuthUIAuthenticationResult?) {
+        val response = result?.idpResponse
+        if (result != null) {
+            if (result.resultCode == RESULT_OK) {
+                val user = FirebaseAuth.getInstance().currentUser
+                createBottomNavigation()
+            } else {
+                if (response?.error?.errorCode == ErrorCodes.NO_NETWORK) {
+                    //Show No Internet Notification
+                    return
+                }
+
+                if(response?.error?.errorCode == ErrorCodes.UNKNOWN_ERROR) {
+                    Toast.makeText(this, response?.error?.errorCode.toString(), Toast.LENGTH_LONG)
+                        .show()
+                    Log.d("ERRORCODE", response?.error?.errorCode.toString())
+                    return
+                }
+            }
+        }
+    }
+
+    private fun signOut() {
+        AuthUI.getInstance()
+            .signOut(this)
+    }
+
+    private fun createBottomNavigation() {
         navController = findNavController(R.id.navHostFragment)
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                    R.id.recipesFragment,
-                    R.id.favoriteRecipesFragment,
-                    R.id.cartFragment,
-                    R.id.orderFragment
-        ))
+                R.id.recipesFragment,
+                R.id.favoriteRecipesFragment,
+                R.id.cartFragment,
+                R.id.orderFragment
+            ))
 
         bottomNavigationView.setupWithNavController(navController)
         setupActionBarWithNavController(navController, appBarConfiguration)
